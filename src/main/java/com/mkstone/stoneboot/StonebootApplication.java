14de3e2c0610773fd73104ed700f84e2444b6d44
mkStone <993166015@qq.com>
2018-03-30 11:29:49 +0800

    项目

---
+++ src/main/java/com/mkstone/stoneboot/StonebootApplication.java
@@ -0,0 +1,12 @@
+package com.mkstone.stoneboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonebootApplication.class, args);
+	}
+}
